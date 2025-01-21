66c4ab792004f41b0f178987dd3f84314ba6c78b
ahmadtechdev <ahmadtechdev@gmail.com>
2025-01-21 11:38:57 +0500

    bloc integrated data coming for firebase logic integrated but not successful need to check issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:car_rent_app/firebase_options.dart';
+import 'package:car_rent_app/injection_container.dart';
+import 'package:car_rent_app/presentation/bloc/car_bloc.dart';
+import 'package:car_rent_app/presentation/bloc/car_event.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'presentation/pages/onboarding_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  initInjection();
   runApp(const MainApp());
 }
 
@@ -23,8 +28,16 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: OnboardingPage(),
+    return  BlocProvider(
+      create: (_)=>getIt<CarBloc>()..add(LoadCars()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: OnboardingPage(),
+      ),
     );
   }
 }
--- lib/presentation/pages/car_list_screen.dart
+++ lib/presentation/pages/car_list_screen.dart
@@ -1,26 +1,12 @@
+import 'package:car_rent_app/presentation/bloc/car_bloc.dart';
 import 'package:car_rent_app/presentation/widgets/car_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../data/models/car.dart';
+import '../bloc/car_state.dart';
 
 class CarListScreen extends StatelessWidget {
-  CarListScreen({super.key});
-
-  final List<Car> cars = [
-    Car(model: 'Fortuner GR',
-        distance: 870,
-        fuelCapacity: 50,
-        pricePerHour: 45),
-    Car(model: 'Fortuner GR',
-        distance: 870,
-        fuelCapacity: 50,
-        pricePerHour: 45),
-    Car(model: 'Fortuner GR',
-        distance: 870,
-        fuelCapacity: 50,
-        pricePerHour: 45),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,11 +16,21 @@ class CarListScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: ListView.builder(
-          itemCount: cars.length,
-          itemBuilder: (context, index) {
-            return CarCard(car: cars[index]);
-          }),
+      body: BlocBuilder<CarBloc, CarState>(builder: (context, state) {
+        if (state is CarsLoading) {
+          return const Center(child: CircularProgressIndicator(),);
+        } else if (state is CarsLoaded) {
+          return ListView.builder(itemCount: state.cars.length,
+            itemBuilder: (context, index) {
+              return CarCard(car: state.cars[index]);
+            },
+          );
+        }else if( state is CarsError){
+          return Center(child: Text('error: ${state.message}'),);
+        }
+
+        return Container();
+      }),
     );
   }
 }
